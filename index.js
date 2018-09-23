125781ec793014e96f84d05740f049cddf664dc9
demian sims <demiansims@gmail.com>
2018-09-23 12:02:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ function createRock(x) {
      document.addEventListener('keydown', function(e) {
        if (e.which === 37) {
          var leftNumbers = rock.style.left('px', '')
+         var left = 
        }
      }
   }
